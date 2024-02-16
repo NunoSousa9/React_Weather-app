3883654ed766b3a9ea6c00e376e2bc029047ffcd
NunoSousa9 <139639488+NunoSousa9@users.noreply.github.com>
2024-02-16 00:55:09 +0000

    refactoring

--- src/API/GetAPI.jsx
+++ src/API/GetAPI.jsx
@@ -1,6 +1,7 @@
 const fetchCityData = async (lat, lon) => {
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a82f301e4fe13da3d06942a9363cc05b
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=80cf1d0fc1b1038188b741ce2b0f4730
+      
       `);
       console.log('Response status:', response.status);
       if(!response.ok) {
