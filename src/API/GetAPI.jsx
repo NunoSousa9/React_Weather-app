36e9ca320546528abf90698452b6a9a1c97fe100
NunoSousa9 <139639488+NunoSousa9@users.noreply.github.com>
2024-02-22 02:52:39 +0000

    hidden apikey

--- src/API/GetAPI.jsx
+++ src/API/GetAPI.jsx
@@ -1,18 +1,32 @@
 const fetchCityData = async (lat, lon) => {
+
+  const apiKeys = [
+    process.env.REACT_APP_API_KEY_1,
+    process.env.REACT_APP_API_KEY_2,
+    process.env.REACT_APP_API_KEY_3
+  ].filter(Boolean);
+
+  for (let i = 0; i < apiKeys.length; i++) {
+
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a82f301e4fe13da3d06942a9363cc05b
-      
-      `);
+      console.log(`Trying API key: ${i + 1}`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeys[i]}`);
       console.log('Response status:', response.status);
-      if(!response.ok) {
+      
+      if (!response.ok) {
         throw new Error('Failed to fetch city data');
       }
       const data = await response.json();
       return [data];
     } catch (error) {
-      console.error('Error fetching city data:', error);
-      return [];
+      console.error(`Error fetching city data with API key: ${i + 1} )`, error);
+      // If the current key fails, proceed to the next one
+      continue;
     }
-  };
+  }
 
-  export { fetchCityData };
+  console.error("All API keys failed.");
+  return []; // If all API keys fail
+};
+
+export { fetchCityData };
